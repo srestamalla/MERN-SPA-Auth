e46597feda6783a18cf8d583aca8d4ec8f0f2c82
Sresta Malla <mallasresta@gmail.com>
2024-01-09 03:12:51 +0545

    added jwt token, authentication and user's authentication endpoints

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -22,7 +22,7 @@ const userSchema = mongoose.Schema(
   }
 );
 
-//hash the password using bcrypt
+// hash the password using bcrypt
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
@@ -30,6 +30,11 @@ userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// validate password using bcrypt
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
