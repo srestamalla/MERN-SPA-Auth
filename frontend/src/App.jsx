9d7c5ce16c48bd0a4b2375704db67feeea8d1392
Sresta Malla <mallasresta@gmail.com>
2024-01-09 17:49:55 +0545

    initializing frontend of the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <h1>MERN SPA Auth</h1>
+    </>
+  );
+};
+
+export default App;
